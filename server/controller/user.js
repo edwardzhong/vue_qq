9936e83b3133940e60522ea122e110a84747e362
edwardzhong <zhongjfgd@163.com>
2020-07-07 17:23:08 +0800

    format

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,133 +1,134 @@
-const mysql = require('mysql')
-const { stringFormat } = require('../common/util')
-const userDao = require('../daos/user')
-const { getFriends } = require('../daos/friend')
-const { getApply } = require('../daos/apply')
-const { getGroup,getGroupUsers,getGroupMsg } = require('../daos/group')
-const { getReads, clearRead, getUserMsg } = require('../daos/message')
+const mysql = require('mysql');
+const { stringFormat } = require('../common/util');
+const userDao = require('../daos/user');
+const { getFriends } = require('../daos/friend');
+const { getApply } = require('../daos/apply');
+const { getGroup, getGroupUsers, getGroupMsg } = require('../daos/group');
+const { getReads, clearRead, getUserMsg } = require('../daos/message');
 
 // exports.index = async function (ctx) {
 //     ctx.render('index.html');
 // };
 
-const formatTime = i => {
-    const d = new Date(i.create_date * 1000),
-        n = new Date(),
-        day = n.getDate() - d.getDate(),
-        date = ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2) + ':' + ('0' + d.getSeconds()).slice(-2);
-    if (day == 0) {
-        i.date = `${date}`
-    } else if (day == 1) {
-        i.date = `昨天 ${date}`
-    } else {
-        i.date = `${d.getFullYear()}-${d.getMonth()}-${d.getDate()} ${date}`
-    }
-    return i;
+const formatTime = (i) => {
+  const d = new Date(i.create_date * 1000),
+    n = new Date(),
+    day = n.getDate() - d.getDate(),
+    date = ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2) + ':' + ('0' + d.getSeconds()).slice(-2);
+  if (day == 0) {
+    i.date = `${date}`;
+  } else if (day == 1) {
+    i.date = `昨天 ${date}`;
+  } else {
+    i.date = `${d.getFullYear()}-${d.getMonth()}-${d.getDate()} ${date}`;
+  }
+  return i;
 };
 
 const mergeReads = (list, reads) => {
-    return list.map(l => {
-        const obj = reads.find(r => r.id == l.id);
-        l.reads = obj ? obj.count : 0
-        return l;
-    });
+  return list.map((l) => {
+    const obj = reads.find((r) => r.id == l.id);
+    l.reads = obj ? obj.count : 0;
+    return l;
+  });
 };
 
-exports.getInfo = async function (ctx) {
-    const token = await ctx.verify();
-    const [users, friends, groups, applys, reads] = await Promise.all([
-        userDao.getUser({ id: token.uid }), 
-        getFriends([token.uid]), 
-        getGroup([token.uid]), 
-        getApply([token.uid]), 
-        getReads([token.uid])
-    ]);
+exports.getInfo = async function(ctx) {
+  const token = await ctx.verify();
+  const [users, friends, groups, applys, reads] = await Promise.all([
+    userDao.getUser({ id: token.uid }),
+    getFriends([token.uid]),
+    getGroup([token.uid]),
+    getApply([token.uid]),
+    getReads([token.uid]),
+  ]);
 
-    const msgs = applys.map(formatTime);
-    ctx.body = {
-        code: 0,
-        message: '好友列表',
-        data: { user: users[0], friends: mergeReads(friends, reads), groups, msgs }
-    };
-}
+  const msgs = applys.map(formatTime);
+  ctx.body = {
+    code: 0,
+    message: '好友列表',
+    data: { user: users[0], friends: mergeReads(friends, reads), groups, msgs },
+  };
+};
 
-exports.updateInfo = async function (ctx) {
-    const form = ctx.request.body;
-    const token = await ctx.verify();
-    const ret = await userDao.update([form, token.uid]);
-    if (!ret.affectedRows) {
-        return ctx.body = {
-            code: 2,
-            message: '更新失败'
-        };
-    }
-    ctx.body = {
-        code: 0,
-        message: '更新成功'
-    };
-}
-
-exports.getMsg = async function (ctx) {
-    const { id,reads } = ctx.query;
-    const { uid } = await ctx.verify();
-    const list = await getUserMsg([id, uid, uid, id]);
-    const msgs = list.map(formatTime).map(l => {
-        l.msg = l.content;
-        l.self = l.send_id == uid;
-        return l;
+exports.updateInfo = async function(ctx) {
+  const form = ctx.request.body;
+  const token = await ctx.verify();
+  const ret = await userDao.update([form, token.uid]);
+  if (!ret.affectedRows) {
+    return (ctx.body = {
+      code: 2,
+      message: '更新失败',
     });
-    if(Number(reads)){
-        clearRead([uid, id]);
-    }
-    ctx.body = {
-        code: 0,
-        message: '消息列表',
-        data: msgs
-    };
-}
+  }
+  ctx.body = {
+    code: 0,
+    message: '更新成功',
+  };
+};
 
-exports.getGroupInfo = async function (ctx) {
-    const { id } = ctx.query;
-    const { uid } = await ctx.verify();
-    const [users, list] = await Promise.all([getGroupUsers([id]), getGroupMsg([id])]);
-    const msgs = list.map(formatTime).map(l => {
-        l.msg = l.content;
-        l.self = l.send_id == uid;
-        return l;
+exports.getMsg = async function(ctx) {
+  const { id, reads } = ctx.query;
+  const { uid } = await ctx.verify();
+  const list = await getUserMsg([id, uid, uid, id]);
+  const msgs = list.map(formatTime).map((l) => {
+    l.msg = l.content;
+    l.self = l.send_id == uid;
+    return l;
+  });
+  if (Number(reads)) {
+    clearRead([uid, id]);
+  }
+  ctx.body = {
+    code: 0,
+    message: '消息列表',
+    data: msgs,
+  };
+};
+
+exports.getGroupInfo = async function(ctx) {
+  const { id } = ctx.query;
+  const { uid } = await ctx.verify();
+  const [users, list] = await Promise.all([getGroupUsers([id]), getGroupMsg([id])]);
+  const msgs = list.map(formatTime).map((l) => {
+    l.msg = l.content;
+    l.self = l.send_id == uid;
+    return l;
+  });
+  ctx.body = {
+    code: 0,
+    message: '获取组信息成功',
+    data: { users, msgs },
+  };
+};
+
+exports.delFriend = async function(ctx) {
+  const { friend_id } = ctx.request.body;
+  const token = await ctx.verify();
+  const sql = stringFormat( "delete from user_friend where user_id in ('$1','$2') and friend_id in ('$1','$2')", token.uid, friend_id, );
+  const ret = await userDao.sql(sql);
+  if (!ret.affectedRows) {
+    return (ctx.body = {
+      code: 2,
+      message: '删除好友失败',
     });
-    ctx.body = {
-        code: 0,
-        message: '获取组信息成功',
-        data: { users, msgs }
-    };
-}
+  }
+  ctx.body = {
+    code: 0,
+    message: '删除好友成功',
+  };
+};
 
-exports.delFriend = async function (ctx) {
-    const { friend_id } = ctx.request.body;
-    const token = await ctx.verify();
-    const sql = stringFormat("delete from user_friend where user_id in ('$1','$2') and friend_id in ('$1','$2')", token.uid, friend_id);
-    const ret = await userDao.sql(sql);
-    if (!ret.affectedRows) {
-        return ctx.body = {
-            code: 2,
-            message: '删除好友失败'
-        };
-    }
-    ctx.body = {
-        code: 0,
-        message: '删除好友成功'
-    };
-}
-
-exports.search = async function (ctx) {
-    const { kw } = ctx.query;
-    const k1 = mysql.escape(kw + '%'), k2 = mysql.escape('%' + kw + '%');
-    const sql = stringFormat("select * from user where name like $1 or name like $2 or nick like $1 or nick like $2 or cast(num as char) like $1", k1, k2);
-    const gSql = stringFormat("select a.*,b.nick as create_name from `group` a left join `user` b on a.create_id = b.id where a.name like $1 or b.name like $2", k1, k2);
-    const [users, groups] = await Promise.all([userDao.sql(sql), userDao.sql(gSql)]);
-    ctx.body = {
-        code: 0,
-        message: '搜索成功',
-        data: { users, groups }
-    };
-}
+exports.search = async function(ctx) {
+  const { kw } = ctx.query;
+  const k1 = mysql.escape(kw + '%'),
+    k2 = mysql.escape('%' + kw + '%');
+  const sql = stringFormat( 'select * from user where name like $1 or name like $2 or nick like $1 or nick like $2 or cast(num as char) like $1', k1, k2, );
+  const gSql = stringFormat( 'select a.*,b.nick as create_name from `group` a left join `user` b on a.create_id = b.id where a.name like $1 or b.name like $2', k1, k2, );
+  const [users, groups] = await Promise.all([userDao.sql(sql), userDao.sql(gSql)]);
+  ctx.body = {
+    code: 0,
+    message: '搜索成功',
+    data: { users, groups },
+  };
+};
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,139 +1,143 @@
-const auth = require('./auth.js')
+const auth = require('./auth.js');
 const { insertMsg, insertToUser } = require('../daos/message');
-const log = require('../common/logger')
+const log = require('../common/logger');
 
 let MAP = {};
 let LIST = [];
 let ROOMS = [];
-const findUid = sid => {
-    let id = '';
-    for (let [k, v] of Object.entries(MAP)) {
-        if (v == sid) {
-            id = k;
-        }
+const findUid = (sid) => {
+  let id = '';
+  for (let [k, v] of Object.entries(MAP)) {
+    if (v == sid) {
+      id = k;
     }
-    return id;
+  }
+  return id;
 };
-const deleteUser = sid => {
-    let i = LIST.findIndex(l => l.socketId == sid);
-    if (i > - 1) {
-        delete MAP[LIST[i].id];
-    }
-    return LIST.splice(i, 1)[0];
+const deleteUser = (sid) => {
+  let i = LIST.findIndex((l) => l.socketId == sid);
+  if (i > -1) {
+    delete MAP[LIST[i].id];
+  }
+  return LIST.splice(i, 1)[0];
 };
 
-const addRooms = arr => {
-    arr.forEach(a => {
-        if (!ROOMS.includes(a)) {
-            ROOMS.push(a);
-        }
-    });
-}
+const addRooms = (arr) => {
+  arr.forEach((a) => {
+    if (!ROOMS.includes(a)) {
+      ROOMS.push(a);
+    }
+  });
+};
 
 const currTime = () => {
-    const d = new Date(), date = `${d.getFullYear()}-${d.getMonth()}-${d.getDate()}`;
-    return ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2) + ':' + ('0' + d.getSeconds()).slice(-2);
+  const d = new Date(),
+    date = `${d.getFullYear()}-${d.getMonth()}-${d.getDate()}`;
+  return (
+    ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2) + ':' + ('0' + d.getSeconds()).slice(-2)
+  );
 };
 
-module.exports = io => {
-    // middleware
-    io.use(auth);
-    //namespace (/)
-    io.on('connection', socket => {
-        socket.emit('open', {
-            code: 0,
-            handshake: socket.handshake,
-            namespace: '/',
-            message: 'welcome to main channel, please sign'
-        });
-
-        socket.on('sign', ({ user, rooms }, fn) => {
-            if (!user.id) {
-                return fn({ code: 2, message: 'id not exist' });
-            }
-            MAP[user.id] = socket.id;
-            user.socketId = socket.id;
-            LIST.push(user);
-            addRooms(rooms);
-
-            socket.join(rooms);//加入自己所在的组
-            socket.emit('userin', MAP, user);
-            socket.broadcast.emit('userin', MAP, user);
-
-            fn({
-                code: 0,
-                message: 'sign success',
-                data: MAP
-            });
-        });
-
-        //两人聊天
-        socket.on('send', async (uid, msg) => {
-            const sid = MAP[uid];//接收用户socket.id
-            const cid = findUid(socket.id);//发送用户id
-
-            if (sid) { // 好友在线则发送
-                socket.to(sid).emit('reply', { id: cid, self: false }, { date: currTime(), msg });
-            }
-            // 给自己也发一份
-            socket.emit('reply', { id: uid, self: true }, { date: currTime(), msg });
-            // 保存数据库
-            try {
-                const ret = await insertMsg({ send_id: cid, receive_id: uid, content: msg });
-                insertToUser({ user_id: uid, send_id: cid, message_id: ret.insertId, is_read: sid ? 1 : 0 });
-            } catch (err) {
-                log.error(err);
-            }
-        });
-
-        //群组聊天
-        socket.on('groupSend', async ({gid,user}, msg) => {
-            socket.to(gid).broadcast.emit('groupReply', { gid: gid , ...user }, { date: currTime(), msg });
-            // 给自己也发一份
-            socket.emit('groupReply', { gid: gid , ...user }, { date: currTime(), msg });
-
-            //保存数据库
-            try {
-                const ret = await insertMsg({ type:1, send_id: user.id, group_id: gid, content: msg });
-                insertToUser({ group_id: gid, send_id: user.id, message_id: ret.insertId, is_read: 1 });
-            } catch (err) {
-                log.error(err);
-            }
-        });
-
-        socket.on('acceptFriend', (uid) => {
-            const sid = MAP[uid];
-            if (sid) {
-                socket.to(sid).emit('refresh', MAP);
-            }
-            socket.emit('checkStatus', MAP);
-        });
-
-        socket.on('sendApply', (uid, data) => {
-            const sid = MAP[uid];
-            if(sid){
-                socket.to(MAP[uid]).emit('apply', { ...data, date: currTime() });
-            }
-        });
-
-        socket.on('disconnect', () => {
-            const user = deleteUser(socket.id);
-            socket.broadcast.emit('userout', MAP, user);
-        });
+module.exports = (io) => {
+  // middleware
+  io.use(auth);
+  //namespace (/)
+  io.on('connection', (socket) => {
+    socket.emit('open', {
+      code: 0,
+      handshake: socket.handshake,
+      namespace: '/',
+      message: 'welcome to main channel, please sign',
     });
 
-    // // namespace (/chat) 
-    // const chat = io.of('/chat').on('connection', socket => {
-    //     socket.emit('open', {
-    //         code: 0,
-    //         namespace: '/',
-    //         message: 'welcome to chat channel '
-    //     });
+    socket.on('sign', ({ user, rooms }, fn) => {
+      if (!user.id) {
+        return fn({ code: 2, message: 'id not exist' });
+      }
+      MAP[user.id] = socket.id;
+      user.socketId = socket.id;
+      LIST.push(user);
+      addRooms(rooms);
 
-    //     socket.on('message', data => {
+      socket.join(rooms); //加入自己所在的组
+      socket.emit('userin', MAP, user);
+      socket.broadcast.emit('userin', MAP, user);
 
-    //     });
-    // });
-    // 
-    // chat.use(auth);
+      fn({
+        code: 0,
+        message: 'sign success',
+        data: MAP,
+      });
+    });
+
+    //两人聊天
+    socket.on('send', async (uid, msg) => {
+      const sid = MAP[uid]; //接收用户socket.id
+      const cid = findUid(socket.id); //发送用户id
+
+      if (sid) {
+        // 好友在线则发送
+        socket.to(sid).emit('reply', { id: cid, self: false }, { date: currTime(), msg });
+      }
+      // 给自己也发一份
+      socket.emit('reply', { id: uid, self: true }, { date: currTime(), msg });
+      // 保存数据库
+      try {
+        const ret = await insertMsg({ send_id: cid, receive_id: uid, content: msg });
+        insertToUser({ user_id: uid, send_id: cid, message_id: ret.insertId, is_read: sid ? 1 : 0 });
+      } catch (err) {
+        log.error(err);
+      }
+    });
+
+    //群组聊天
+    socket.on('groupSend', async ({ gid, user }, msg) => {
+      socket.to(gid).broadcast.emit('groupReply', { gid: gid, ...user }, { date: currTime(), msg });
+      // 给自己也发一份
+      socket.emit('groupReply', { gid: gid, ...user }, { date: currTime(), msg });
+
+      //保存数据库
+      try {
+        const ret = await insertMsg({ type: 1, send_id: user.id, group_id: gid, content: msg });
+        insertToUser({ group_id: gid, send_id: user.id, message_id: ret.insertId, is_read: 1 });
+      } catch (err) {
+        log.error(err);
+      }
+    });
+
+    socket.on('acceptFriend', (uid) => {
+      const sid = MAP[uid];
+      if (sid) {
+        socket.to(sid).emit('refresh', MAP);
+      }
+      socket.emit('checkStatus', MAP);
+    });
+
+    socket.on('sendApply', (uid, data) => {
+      const sid = MAP[uid];
+      if (sid) {
+        socket.to(MAP[uid]).emit('apply', { ...data, date: currTime() });
+      }
+    });
+
+    socket.on('disconnect', () => {
+      const user = deleteUser(socket.id);
+      socket.broadcast.emit('userout', MAP, user);
+    });
+  });
+
+  // // namespace (/chat)
+  // const chat = io.of('/chat').on('connection', socket => {
+  //     socket.emit('open', {
+  //         code: 0,
+  //         namespace: '/',
+  //         message: 'welcome to chat channel '
+  //     });
+
+  //     socket.on('message', data => {
+
+  //     });
+  // });
+  //
+  // chat.use(auth);
 };
